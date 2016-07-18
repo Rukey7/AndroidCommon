fae3b4915d8a02923440d508327157a1937ecb0c
Rukey7 <deadlong7@gmail.com>
2016-07-18 22:43:05 +0800

    更新

---
+++ commonlib/src/main/java/com/dl7/commonlib/utils/MeasureUtils.java
@@ -0,0 +1,24 @@
+package com.dl7.commonlib.utils;
+
+import android.content.Context;
+import android.util.TypedValue;
+
+/**
+ * Created by Rukey7 on 2016/7/18.
+ */
+public class MeasureUtils {
+
+    private MeasureUtils() {
+    }
+
+
+    public static float dp2px(Context context, float dp) {
+        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp,
+                context.getResources().getDisplayMetrics());
+    }
+
+    public static float sp2px(Context context, float sp) {
+        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, sp,
+                context.getResources().getDisplayMetrics());
+    }
+}
